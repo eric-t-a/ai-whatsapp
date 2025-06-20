477209424806a2b52e07170a5da534bdae765ace
eric-t-a <eric.exts@gmail.com>
2025-06-20 15:35:21 -0300

    support to whatsapp official api

---
+++ src/class/WhatsappAPI.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+
+export interface MsgReceivedBody {
+    object: string;
+    entry: MsgEntry[]
+}
+
+interface MsgEntry {
+    id: string;
+    changes: MsgChange[]
+}
+
+interface MsgChange {
+    value: {
+        messaging_product: string;
+        metadata: {
+            display_phone_number: string;
+            phone_number_id: string;
+        };
+        contacts: MsgContact[];
+        messages?: Msg[];
+        statuses?: MsgStatus[];
+    };
+    field: string;
+}
+
+interface MsgStatus {
+    id: string;
+    status: string;
+    timestamp: string;
+    recipient_id: string;
+}
+
+interface MsgContact {
+    profile: { name?: string };
+    wa_id: string;
+}
+
+interface Msg {
+    from: string;
+    id: string;
+    timestamp: string;
+    type: 'text' | 'audio' | 'image' | 'document' | 'interactive'
+    document?: { filename: string; id: string; mime_type: string; }
+    audio?: { id: string; mime_type: string; }
+    image?: { id: string; mime_type: string; }
+    text?: { body: string; }
+    interactive?: {
+        type: 'string', 
+        button_reply?: { id: string; title: string; }
+    }
+}
+
+
+export class WhatsAppAPI {
+
+    constructor () {
+    }
+
+    async startTyping(msgId: string) {
+        await this.axios_base({
+            messaging_product: "whatsapp",
+            status: "read",
+            message_id: msgId,
+            typing_indicator: {
+              type: "text"
+            }
+        })
+    }
+
+    async stopTyping(to: string) {
+
+    }
+
+    async sendTextMessage(to: string, content: string) {
+        await this.axios_base({
+            messaging_product: "whatsapp",    
+            recipient_type: "individual",
+            to: to,
+            type: "text",
+            text: {
+                body: content
+            }
+        })
+    }
+
+    async axios_base(data: object) {
+        await axios({
+            url: `https://graph.facebook.com/${process.env.WA_VERSION}/${process.env.WA_PHONE_NUMBER_ID}/messages`,
+            method: 'post',
+            headers: {
+              'Authorization': `Bearer ${process.env.WA_ACCESS_TOKEN}`,
+              'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(data)
+          })
+    }
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { WhatsAppWeb } from './class/WhatsappWeb';
 import Whisper from "node-speech-recognition";
-import { AI } from './class/ai';
+import { AI } from './class/AI';
+import { MsgReceivedBody, WhatsAppAPI } from './class/WhatsappAPI';
 dotenv.config();
 
 const app = express();
+app.use(express.json())
 const port = process.env.PORT || 3000;
-var ai: InstanceType<typeof AI>, wpp: InstanceType<typeof WhatsAppWeb>, whisper: InstanceType<typeof Whisper>;
+var ai: InstanceType<typeof AI>, wpp: InstanceType<typeof WhatsAppWeb | typeof WhatsAppAPI>, whisper: InstanceType<typeof Whisper>;
 
 async function init() {
     ai = new AI();
-    wpp = new WhatsAppWeb(onMessageReceived);
+    // wpp = new WhatsAppWeb(onMessageReceived);
+    wpp = new WhatsAppAPI;
     whisper = new Whisper();
     await whisper.init('base');
 }
@@ -39,8 +42,28 @@ async function onMessageReceived(from: string, metadata: Record<string, string>)
     await wpp.stopTyping(from)
 }
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`Hello, TypeScript Express!`);
+app.get('/webhook', (req: Request, res: Response) => {
+    res.status(200).send(req.query['hub.challenge']);
+});
+
+app.post('/webhook', async (req: Request, res: Response) => {
+    const body = req.body as MsgReceivedBody;
+    const messages = body.entry[0].changes[0].value.messages;
+
+    if(!messages || !messages.length) return;
+
+    const msg = messages[0].text?.body;
+    const btn = messages[0].interactive?.button_reply?.title;
+    const from = messages[0].from;
+
+    if(!msg) return;
+    await wpp.startTyping(messages[0].id);
+    
+    const response = await ai.answer(msg)
+    const data = await wpp.sendTextMessage(from, response)
+    await wpp.stopTyping(from)
+
+    res.status(200).send('ok');
 });
 
 app.listen(port, () => {
