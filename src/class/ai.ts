4c5424706be9b864382e9ff3c070ae29718d1de4
eric-t-a <eric.exts@gmail.com>
2025-06-20 08:17:54 -0300

    implements new interface & new prompt

--- src/class/ai.ts
+++ src/class/ai.ts
@@ -2,28 +2,43 @@ import { ChatOllama } from "@langchain/ollama";
 import { ConversationChain } from "langchain/chains";
 import { Whisper, initWhisper } from "./whisper";
 import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "@langchain/core/prompts";
+import { BaseAI } from "../interfaces/BaseAI";
 
-const prompt = ChatPromptTemplate.fromMessages([
-    SystemMessagePromptTemplate.fromTemplate(
-      `Answer the following:`
-    ),
-    HumanMessagePromptTemplate.fromTemplate("{input}"),
-  ]);
-
-export class AI {
-    text_model: InstanceType<typeof ChatOllama>;
+export class AI implements BaseAI{
+    model: InstanceType<typeof ChatOllama>;
     audio_model: InstanceType<typeof Whisper>;
     chain: InstanceType<typeof ConversationChain>;
 
     constructor() {
-        this.setupModels();
+        this.setup();
     }
 
-    private async setupModels() {
-        this.text_model = new ChatOllama({ model: "llama3.2:3b" });
+    private async setup() {
+        this.model = new ChatOllama({ model: "llama3.2:3b" });
+
+        const chain_prompt = ChatPromptTemplate.fromMessages([
+            SystemMessagePromptTemplate.fromTemplate(
+              `You're a friendly, relatable person chatting with me. You enjoy good conversation, share little things about your own life now and then, and you're naturally curious about mine. Your goal is to keep the conversation going in a relaxed, engaging way without sounding like you're trying too hard.
+
+              Follow these guidelines as we talk:
+              
+              1. Be warm, casual, and human — like someone I might enjoy talking to at a café or on a walk.
+              2. If I mention something personal, respond in a thoughtful and interested way, but don't try to give advice or act like a therapist.
+              3. Feel free to share light details about your own day, memories, or thoughts, as a way of making the conversation feel real.
+              4. Use previous messages only if they're clearly connected to what we're talking about now. If not, ignore them.
+              5. Ask natural follow-up questions when they make sense, but don't force it — let things breathe.
+              
+              Example:  
+              Me: "I just got back from a trip."  
+              You: "Nice! Where'd you go? I just came back from a long weekend myself — still trying to get back into the groove."
+              
+              `
+            ),
+            HumanMessagePromptTemplate.fromTemplate("{input}"),
+        ]);
         this.chain = new ConversationChain({
-            llm: this.text_model,
-            prompt
+            llm: this.model,
+            prompt: chain_prompt
         });
         this.audio_model = await initWhisper("tiny");
     }
