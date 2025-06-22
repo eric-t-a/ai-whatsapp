486bdbb93fafee72e6f177119f9411e2854dd90c
eric-t-a <eric.exts@gmail.com>
2025-06-22 20:23:51 -0300

    get messages route

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { Msg, MsgReceivedBody } from '../class/WhatsappAPI';
 import { ai, wpp } from '../app';
-import { Message, MsgType, saveNewMessage } from '../models/message';
+import { Message, MessageModel, MsgType, saveNewMessage } from '../models/message';
 import { Recipient, RecipientModel } from '../models/recipient';
 
 const router = express.Router();
@@ -11,6 +11,14 @@ router.get('/', async (req: Request, res: Response) => {
     const recipients = await RecipientModel.find().populate('lastMsg').sort({ lastMsgSentTime: -1 }).skip(skip).limit(20);
     
     res.status(200).send({recipients});
+});
+
+router.get('/:id/messages', async (req: Request, res: Response) => {
+    const recipientId = req.params.id;
+    const skip = parseInt(req.query.skip?.toString() || '0', 10);
+    const messages = await MessageModel.find({ recipient: recipientId }).sort({ sentTime: -1 }).skip(skip).limit(20);
+    
+    res.status(200).send({messages});
 });
 
 router.post('/', async (req: Request, res: Response) => {
